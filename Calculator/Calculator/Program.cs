ab2e52c418043fa1f67ef25d7d76244cd64eefc8
PhucNguyenHong <phucnhhe172629@fpt.edu.vn>
2024-01-06 10:27:36 +0700

    first commit

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //get number
+                (int a, int b) numbers = getNumber();
+                //get operation
+                int oper = getOperation();
+                //Get result
+                getResult(numbers, oper);
+            }
+        }
+
+        private static void getResult((int a, int b) numbers, int oper)
+        {
+            switch (oper)
+            {
+                case 1:
+                    Console.WriteLine($"{numbers.a} + {numbers.b} = {numbers.a + numbers.b}");
+                    break;
+                case 2:
+                    Console.WriteLine($"{numbers.a} - {numbers.b} = {numbers.a - numbers.b}");
+                    break;
+                case 3:
+                    Console.WriteLine($"{numbers.a} * {numbers.b} = {numbers.a * numbers.b}");
+                    break;
+                case 4:
+                    Console.WriteLine($"{numbers.a} / {numbers.b} = {numbers.a / numbers.b}");
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        private static int getOperation()
+        {
+            Console.WriteLine("Choose operation:\r\n" +
+                "1 - Addition\r\n" +
+                "2 - Subtraction\r\n" +
+                "3 - Multiplication\r\n" +
+                "4 - Division\r\n" +
+                "0 - Exit\r\n");
+            return int.Parse(Console.ReadLine());
+        }
+
+        private static (int a, int b) getNumber()
+        {
+            Console.Write("Enter a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter b = ");
+            int b = int.Parse(Console.ReadLine());
+            return (a, b);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, b, c;
+            Console.Write("Input a = ");
+            a = Double.Parse(Console.ReadLine());
+            Console.Write("Input b = ");
+            b = Double.Parse(Console.ReadLine());
+            Console.Write("Input c = ");
+            c = Double.Parse(Console.ReadLine());
+            if (a != 0)
+            {
+                Double del = b * b - 4 * a * c;
+                if (del < 0)
+                {
+                    Console.WriteLine("VN");
+                }
+                else if (del == 0) 
+                {
+                    Console.WriteLine("x1 = x2 = " + (-b / (2 * a)));
+                }
+                else if (del > 0)
+                {
+                    Console.WriteLine("x1 = " + ((-b + Math.Sqrt(del)) / (2 * a)));
+                    Console.WriteLine("x2 = " + ((-b - Math.Sqrt(del)) / (2 * a)));
+                }
+            }
+            else
+            {
+                if (b != 0)
+                {
+                    Console.WriteLine("x = " + (-c / b));
+                }
+                else if (c == 0)
+                {
+                    Console.WriteLine("VSN");
+                }
+                else
+                {
+                    Console.WriteLine("VN");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
